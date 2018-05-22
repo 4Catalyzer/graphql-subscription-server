b44c08ee0cb2ce7dea40b096458104b0e095e8b1
Jason Quense <monastic.panic@gmail.com>
2018-05-22 13:47:55 -0400

    feat: use native async generators/iterators (#16)
    * feat: use native async generators/iterators
    
    * chore: Update package.json

--- examples/todo/tools/create-token.js
+++ examples/todo/tools/create-token.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const jwt = require('jsonwebtoken');
 
 // sign with RSA SHA256
-const key = fs.readFileSync(`${__dirname}/private-key.pem`);
+const key = fs.readFileSync(path.join(__dirname, '/private-key.pem'));
 
 const token = jwt.sign({ sub: 'Jimmy' }, key, {
   algorithm: 'RS256',
@@ -10,7 +11,9 @@ const token = jwt.sign({ sub: 'Jimmy' }, key, {
   keyid: 'graphql-subscriptions-test-key',
 });
 
-console.log(`
-Your JWT is:
-  ${token}
-`);
+fs.writeFileSync(
+  path.resolve(__dirname, '../token.json'),
+  JSON.stringify({ token }, null, 2),
+);
+
+console.log('writing token to token.json');
