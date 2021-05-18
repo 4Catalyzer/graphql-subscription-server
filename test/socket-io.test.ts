ed71098a1ef1b6e2edc9b9e0ceadae7e441a6202
Jason Quense <monastic.panic@gmail.com>
2021-05-18 15:20:32 -0400

    feat!: add websocket server option
    BREAKING CHANGE: the subscription server export is now an abstract class

---
+++ test/socket-io.test.ts
@@ -0,0 +1,136 @@
+import socketio from 'socket.io-client';
+
+import SocketIOSubscriptionServer from '../src/SocketIOSubscriptionServer';
+import schema from './data/schema';
+import {
+  TestClient,
+  TestCredentialsManager,
+  graphql,
+  startServer,
+} from './helpers';
+
+function createServer(subscriber) {
+  return new SocketIOSubscriptionServer({
+    path: '/graphql',
+    schema,
+    subscriber,
+    hasPermission: (_, creds) => {
+      return creds !== null;
+    },
+    createCredentialsManager: () => new TestCredentialsManager(),
+    // createLogger: () => console.debug,
+  });
+}
+
+type PromiseType<P> = P extends Promise<infer R> ? R : never;
+
+describe('socket-io client', () => {
+  let server: PromiseType<ReturnType<typeof startServer>>;
+  let client: TestClient | null = null;
+
+  async function createClient(query: string, variables: any) {
+    client = new TestClient(server.subscriber, query, variables);
+
+    await client.init();
+    return client;
+  }
+
+  beforeAll(async () => {
+    server = await startServer(createServer);
+  });
+
+  afterEach(() => {
+    client?.close();
+    client = null;
+  });
+
+  afterAll(async () => {
+    client?.close();
+    await server.close();
+  });
+
+  it('should connect', (done) => {
+    const socket = socketio('http://localhost:5000', {
+      path: '/graphql',
+      transports: ['websocket'],
+    });
+
+    socket.on('connect', () => {
+      socket.close();
+      done();
+    });
+  }, 10000);
+
+  it('should subscribe', async () => {
+    const socket = await createClient(
+      graphql`
+        subscription TestTodoUpdatedSubscription(
+          $input: TodoUpdatedSubscriptionInput!
+        ) {
+          todoUpdated(input: $input) {
+            todo {
+              text
+            }
+          }
+        }
+      `,
+      {
+        input: {
+          id: '1',
+        },
+      },
+    );
+
+    await socket.authenticate();
+
+    expect(
+      await socket.getSubscriptionResult({
+        topic: `todo:1:updated`,
+        data: {
+          id: '1',
+          text: 'Make work',
+        },
+      }),
+    ).toMatchInlineSnapshot(`
+      Object {
+        "event": "subscription update",
+        "payload": Object {
+          "data": Object {
+            "todoUpdated": Object {
+              "todo": Object {
+                "text": "Buy a unicorn",
+              },
+            },
+          },
+          "id": "foo",
+        },
+      }
+    `);
+  });
+
+  it('should unsubscribe', async () => {
+    const socket = await createClient(
+      graphql`
+        subscription TestTodoUpdatedSubscription(
+          $input: TodoUpdatedSubscriptionInput!
+        ) {
+          todoUpdated(input: $input) {
+            todo {
+              text
+            }
+          }
+        }
+      `,
+      {
+        input: {
+          id: '1',
+        },
+      },
+    );
+
+    await socket.authenticate();
+    await socket.subscribe();
+
+    await socket.unsubscribe();
+  });
+});
